e53fdc243c23f9036cd887208fa3aba20c5c7990
Cringenut <ihnatbaikou@gmail.com>
2025-03-06 22:10:16 +0100

    Added spring backend

---
+++ Backend/furniture-shop-app/src/main/java/com/example/furniture_shop_app/FurnitureShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.furniture_shop_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureShopAppApplication.class, args);
+	}
+
+}
